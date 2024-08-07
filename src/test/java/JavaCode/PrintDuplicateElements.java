7d6ecaea6d091b0839188f7fb66598135463ed50
mohitMalikis018 <114010819+mohitMalikis018@users.noreply.github.com>
2024-08-07 15:21:53 +0530

    Utility classes Java Test code

---
+++ src/test/java/JavaCode/PrintDuplicateElements.java
@@ -0,0 +1,22 @@
+package JavaCode;
+
+public class PrintDuplicateElements {
+    public static void main(String args[]){
+        int[] arr = {1,3,5,3,7,9,1,2,2};
+        int[] fr = new int[arr.length];
+        int visited = -1;
+        for(int i = 0;i< arr.length; i++){
+            for (int j = i+1; j< arr.length;j++){
+                if(arr[i] == arr[j]){
+                    fr[j] = visited;
+                    fr[i] = arr[i];
+                }
+            }
+        }
+        for(int i=0;i<fr.length; i++){
+            if(fr[i]!= visited && fr[i]!= 0 ) {
+                System.out.println(fr[i]);
+            }
+        }
+    }
+}
