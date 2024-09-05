99850e78742f042200120abf35fcad2e2e21e7c7
Mohit Malik <mohit1mali@gmail.com>
2024-09-05 18:36:09 +0530

    Collection practice

--- src/test/java/JavaCode/practicePrograms/LeapYear.java
+++ src/test/java/JavaCode/practicePrograms/LeapYear.java
@@ -8,5 +8,31 @@ public class LeapYear {
         boolean isLeap = (year%4 ==0) && (year%100 != 0) || (year%400 ==0);
         System.out.println(isLeap);
 
+        System.out.println("-----");
+
+        System.out.println(isLeapYear(2300));
+
+    }
+
+    public static <T extends Number> boolean isLeapYear(T year) {
+        // Convert the year to an integer
+        int yearValue = year.intValue();
+
+        // Check if the year is divisible by 4
+        if (yearValue % 4 == 0) {
+            // Check if the year is divisible by 100
+            if (yearValue % 100 == 0) {
+                // Check if the year is divisible by 400
+                if (yearValue % 400 == 0) {
+                    return true;
+                } else {
+                    return false;
+                }
+            } else {
+                return true;
+            }
+        } else {
+            return false;
+        }
     }
 }
